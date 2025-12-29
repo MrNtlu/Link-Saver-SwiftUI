fba14fb22cfae6cd9894cc5c6a58cbffa6b45d34
MrNtlu <97burakfidan97@gmail.com>
2025-12-29 16:07:15 +0900

    Initial Commit

---
+++ Link Saver/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Link Saver
+//
+//  Created by Burak on 2025/12/29.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Link Saver/Link_SaverApp.swift
@@ -0,0 +1,32 @@
+//
+//  Link_SaverApp.swift
+//  Link Saver
+//
+//  Created by Burak on 2025/12/29.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Link_SaverApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
